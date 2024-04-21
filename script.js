20f9f508e9184f3b07575237a1394a5d6401c369
Caique-Mendonca <caique4874@gmail.com>
2024-04-20 23:41:31 -0300

    tabela + tratementos de erros 2

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-var botaoCalcular = document.getElementById("botao-calcular")
+let botaoCalcular = document.getElementById("botao-calcular")
 botaoCalcular.addEventListener('click', clicar)
+let inputAltura = document.getElementById("altura");
+let inputPeso = document.getElementById("peso");
+let resultado = document.getElementById("resultado")
 
 function clicar(){
-    var altura = Number(document.getElementById("altura").value) / 100;
-    var peso = Number(document.getElementById("peso").value);
-    var resultado = document.getElementById("resultado")
-    var resultadoIMC = document.getElementById('resultado-imc')
-    var imc = peso / (altura**2)
+    let altura = Number(document.getElementById("altura").value) / 100;
+    let peso = Number(document.getElementById("peso").value);
+    let resultado = document.getElementById("resultado")
+    let resultadoIMC = document.getElementById('resultado-imc')
+    let imc = peso / (altura**2)
     imc = imc.toFixed(2)
 
-    var inputAltura = document.getElementById("altura");
-    var inputPeso = document.getElementById("peso");
-    
-    if(altura == 0 || peso == 0){
-        if(altura == 0 && peso == 0){
+    if(altura == "" || peso == ""){
+        if(altura == "" && peso == ""){
             inputPeso.style.borderColor = "red"
             inputAltura.style.borderColor = "red"
             resultado.innerText = "Por favor preencha todos os campos!"
         }
-        else if(altura == 0){
+        else if(altura == ""){
             inputAltura.style.borderColor = "red"
             inputPeso.style.borderColor = "#636363"
+            resultado.innerText = "Por favor preencha todos os campos!"
         }
-        else if(peso == 0){
+        else if(peso == ""){
             inputPeso.style.borderColor = "red"
             inputAltura.style.borderColor = "#636363"
-
+            resultado.innerText = "Por favor preencha todos os campos!"
         }
     }
     else if(altura < 0 || peso < 0){
-        inputPeso.style.borderColor = "red"
-        inputAltura.style.borderColor = "red"
-        resultado.innerText = "Preencha os campos com números positivos"
+        if(altura < 0 && peso < 0){
+            inputPeso.style.borderColor = "red"
+            inputAltura.style.borderColor = "red"
+            resultado.innerText = "Preencha os campos com números positivos"
+        }
+        else if(altura < 0){
+            inputAltura.style.borderColor = "red"
+            inputPeso.style.borderColor = "#636363"
+            resultado.innerText = "Preencha os campos com números positivos"
+        }
+        else if (peso < 0){
+            inputAltura.style.borderColor = "#636363"
+            inputPeso.style.borderColor = "red"
+            resultado.innerText = "Preencha os campos com números positivos"
+        }
     }
     else{
         inputAltura.style.borderColor = "#636363"
@@ -63,5 +76,12 @@ function clicar(){
         resultadoIMC.innerHTML = `Seu IMC é <strong>${imc}</strong>`
     }
 }
+
+let botaoLimpar = document.querySelector("#Limpar")
+botaoLimpar.addEventListener('click', ()=>{
+    inputAltura.style.borderColor = "#636363"
+    inputPeso.style.borderColor = "#636363"
+    resultado.innerText = ""
+})
 
 
